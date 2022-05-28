4fac356bfee4049a56b53b7c3ac88d5b75f127f5
Xiaopangda <495337099@qq.com>
2022-05-28 14:42:10 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -2,7 +2,32 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram' 
 
+// 把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 设置请求拦截器
+$http.beforeRequest = function(options){
+  // 开启loading效果
+  uni.showLoading({
+      title: '数据加载中...',
+    })
+}
+// 设置响应拦截器
+$http.afterRequest = function(options){
+  // 关闭lodaing效果
+  uni.hideLoading()
+}
+// 封装一个请求失败的提示消息的方法
+uni.$showMsg = function(){
+  uni.showToast({
+    title: '数据加载失败！',
+    duration: 1500,
+    icon: 'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,46 @@
 <template>
   <view>
-    Home
+    <!-- 轮播图的区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,index) in swiperList" :key="index">
+        <!-- 给导航组件动态添加跳转路径携带参数 -->
+        <navigator :url="'/subpkg/goods_detail/goods_detail?good_id='+item.goods_id" class="swiper-item">
+          <!-- 动态绑定图片的 src 属性 -->
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 分类导航的区域 -->
+    <view class="nav-list">
+      <!-- 点击导航分类要进行 tabbar 的分类跳转 使用小程序的 switchTab 方法-->
+      <view class="nav-item" v-for="(item,index) in navList" :key="index" @click="navClickHandler(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+    <!-- 楼层的区域 -->
+    <view class="floor-list">
+      <!-- 楼层的 item 项-->
+      <view class="floor-item" v-for="(item,index) in floorList" :key="index">
+        <!-- 标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 图片展示区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧区域 -->
+          <navigator :url="item.product_list[0].url" class="left-img-box">
+            <image :src="item.product_list[0].image_src" :style="{ width: item.product_list[0].image_width + 'rpx' }"
+              mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧区域 -->
+          <view class="right-img-box">
+            <!-- item 项  第一项不需要-->
+            <navigator :url="product.url" class="right-img-item" v-for="(product,index) in item.product_list"
+              :key="index" v-if="index!==0">
+              <image :src="product.image_src" :style="{ width: product.image_width + 'rpx' }" mode="widthFix"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +48,108 @@
   export default {
     data() {
       return {
-        
+        // 1.1 轮播图的数据列表
+        swiperList: [],
+        // 2.1 分类导航的数据列表
+        navList: [],
+        // 3.1 楼层的数据列表
+        floorList: []
       };
+    },
+    // 页面的生命周期
+    onLoad() {
+      // 1.3 发起请求，获取轮播图的数据
+      this.getSwiperList()
+      // 2.3 发起请求，获取分类导航的数据
+      this.getNavList()
+      // 3.3 发起请求，获取楼层的数据
+      this.getFloorList()
+    },
+    methods: {
+      // 1.2 获取轮播的方法
+      async getSwiperList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // 请求失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功
+        this.swiperList = res.message
+      },
+      // 2.2 获取分类导航的方法
+      async getNavList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/catitems')
+        // 请求失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功
+        this.navList = res.message
+      },
+      // nav-item 项被点击时候的事件处理函数
+      navClickHandler(item) {
+        // 判断点击的是哪个 nav
+        if (item.name === '分类') {
+          uni.switchTab({
+            url: '/pages/cate/cate'
+          })
+        }
+      },
+      // 3.2 获取楼层数据的方法
+      async getFloorList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/floordata')
+        // 请求失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功
+        // 处理图片跳转的地址
+        res.message.forEach(item => {
+          item.product_list.forEach(product => {
+            product.url = '/subpkg/goods_list/goods_list?' + product.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 330rpx;
+  }
 
+  .swiper-item,
+  image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+
+  .floor-title {
+    width: 100%;
+    height: 60rpx;
+  }
+
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
+
+    .right-img-box {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+  }
 </style>
